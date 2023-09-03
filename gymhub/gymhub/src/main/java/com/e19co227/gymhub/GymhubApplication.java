3eee47d5b5645c67e72354c25234a442153ddfe5
Wishula GitHub <e19166@eng.pdn.ac.lk>
2023-09-03 17:13:50 +0530

    Springboot project added

---
+++ gymhub/gymhub/src/main/java/com/e19co227/gymhub/GymhubApplication.java
@@ -0,0 +1,13 @@
+package com.e19co227.gymhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymhubApplication.class, args);
+	}
+
+}
