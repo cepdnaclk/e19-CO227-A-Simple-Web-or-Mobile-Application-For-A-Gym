16f68272803f1a45bd8622942e71d73435db76c4
Wishula GitHub <e19166@eng.pdn.ac.lk>
2023-09-06 22:48:45 +0530

    Header, Hero, Join, Footer basic implementation is finished

---
+++ gymhub/gymhub/src/js/src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Footer.css'
+import Location from "../../assets/location.jpg"
+import Phone from "../../assets/Phone.jpg"
+import Email from "../../assets/email.jpg"
+import Logo from "../../assets/logo.png"
+const Footer = () => {
+  return (
+    <div className="Footer-container">
+    <hr />
+    <div className="footer">
+        <div className="social-links">
+        <img src={Location} alt="" />
+        <img src={Phone} alt="" />
+        <img src={Email} alt="" />
+        </div>
+        
+        <div className="logo-f">
+            <img src={Logo} alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
