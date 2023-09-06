3d63eab0f176b1465c0c3d49df3c34d5ee789c1b
Wishula GitHub <e19166@eng.pdn.ac.lk>
2023-09-06 20:18:39 +0530

    Frontend header and hero section developed

---
+++ gymhub/gymhub/src/js/src/components/Hero/Hero.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './Hero.css'
+import Header from '../Header/Header'
+import backgroundImage from '../../assets/Hero1.jpg'
+
+
+class Hero extends Component {
+    
+  
+    render() {
+      return (
+        <div
+          className="hero"
+          style={{
+            backgroundImage: `url(${backgroundImage})`
+            
+          }}
+        >
+          <div className="left-h">
+              <Header/>
+  
+              {/*hi add*/}
+              <div className='hi-ad'>
+                  <div></div>
+                  <span>Hi This Is GYMHUB - Elevate Your Fitness Journey With Us</span>
+              </div>
+  
+              {/*Hero Heading*/}
+              <div className='hero-text'>
+                  <div><span className='the'>THE</span></div>
+                  <div><span className='stroke-text'>FITNESS HEAVEN </span>
+                  </div>
+                  
+              </div>
+             
+
+             {/*hero buttons*/}
+             
+          </div>
+          <div className="right-h">
+            
+            
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  export default Hero;
